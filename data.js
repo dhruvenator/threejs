f14a7527eb9d55ace99c54d83fcc649c1b9fb7d2
dhruvenator <dhruva@siclarity.com>
2024-07-21 21:23:35 -0700

    first commit

---
+++ data.js
@@ -0,0 +1,339 @@
+export const MUX21X1Data = {
+    "cellName": "MUX21X1",
+    "layers": {
+        "0": {
+            "numShapes": 9,
+            "layerNumber": 400,
+            "layerDT": 0,
+            "data": [[[234, -55], [306, 75]], [[954, -55], [1026, 75]], [[594, -75], [666, 525]], [[774, -75], [846, 525]], [[234, 183], [306, 505]], [[954, 183], [1026, 505]], [[54, -75], [126, 525]], [[1134, -75], [1206, 525]], [[414, -75], [486, 525]]]
+        },
+        "1": {
+            "numShapes": 7,
+            "layerNumber": 402,
+            "layerDT": 0,
+            "data": [[[774, -75], [846, 525]], [[234, -55], [306, 267]], [[954, -55], [1026, 267]], [[414, -75], [486, 525]], [[594, -75], [666, 525]], [[54, -75], [126, 525]], [[1134, -75], [1206, 525]]]
+        },
+        "2": {
+            "numShapes": 6,
+            "layerNumber": 305,
+            "layerDT": 0,
+            "data": [[[870, -59], [930, 509]], [[690, -59], [750, 509]], [[150, -59], [210, 509]], [[510, -59], [570, 509]], [[330, -59], [390, 509]], [[1050, -59], [1110, 509]]]
+        },
+        "3": {
+            "numShapes": 6,
+            "layerNumber": 307,
+            "layerDT": 0,
+            "data": [[[870, -59], [930, 509]], [[510, -59], [570, 327]], [[150, -59], [210, 509]], [[690, 123], [750, 509]], [[330, -59], [390, 509]], [[1050, -59], [1110, 509]]]
+        },
+        "4": {
+            "numShapes": 4,
+            "layerNumber": 404,
+            "layerDT": 0,
+            "data": [[[234, -45], [306, 65]], [[954, -45], [1026, 65]], [[234, 385], [306, 495]], [[954, 385], [1026, 495]]]
+        },
+        "5": {
+            "numShapes": 6,
+            "layerNumber": 701,
+            "layerDT": 0,
+            "data": [[[140, -59], [220, 9]], [[500, -59], [580, 9]], [[320, 241], [400, 309]], [[1040, 341], [1120, 409]], [[680, 441], [760, 509]], [[860, 441], [940, 509]]]
+        },
+        "6": {
+            "numShapes": 2,
+            "layerNumber": 703,
+            "layerDT": 0,
+            "data": [[[680, -59], [760, 9]], [[500, 441], [580, 509]]]
+        },
+        "7": {
+            "numShapes": 6,
+            "layerNumber": 1000,
+            "layerDT": 0,
+            "data": [[[140, -49], [760, -1]], [[320, 251], [400, 299]], [[584, 351], [1120, 399]], [[44, 451], [760, 499]], [[860, 451], [940, 499]], [[1124, 451], [1216, 499]]]
+        },
+        "8": {
+            "numShapes": 6,
+            "layerNumber": 1000,
+            "layerDT": 1,
+            "data": [["S_BAR", 472, 475], ["S", 495, -25], ["G#", 855, 375], ["Q", 1170, 475], ["D1", 900, 475], ["D2", 360, 275]]
+        },
+        "9": {
+            "numShapes": 3,
+            "layerNumber": 700,
+            "layerDT": 0,
+            "data": [[[584, 341], [676, 409]], [[44, 441], [136, 509]], [[1124, 441], [1216, 509]]]
+        },
+        "10": {
+            "numShapes": 2,
+            "layerNumber": 800,
+            "layerDT": 0,
+            "data": [[[0, 375], [1260, 525]], [[0, -75], [1260, 75]]]
+        },
+        "11": {
+            "numShapes": 2,
+            "layerNumber": 800,
+            "layerDT": 1,
+            "data": [["VSS", 630, 450], ["VDD", 630, 0]]
+        },
+        "12": {
+            "numShapes": 1,
+            "layerNumber": 103,
+            "layerDT": 0,
+            "data": [[[0, 183], [1260, 267]]]
+        },
+        "13": {
+            "numShapes": 1,
+            "layerNumber": 100,
+            "layerDT": 0,
+            "data": [[[0, 183], [1260, 267]]]
+        },
+        "14": {
+            "numShapes": 3,
+            "layerNumber": 600,
+            "layerDT": 0,
+            "data": [[[54, -16], [126, 66]], [[594, -16], [666, 66]], [[1134, -16], [1206, 66]]]
+        },
+        "15": {
+            "numShapes": 1,
+            "layerNumber": 10000,
+            "layerDT": 0,
+            "data": [[[0, -75], [1260, 525]]]
+        },
+        "16": {
+            "numShapes": 6,
+            "layerNumber": 2000,
+            "layerDT": 0,
+            "data": [[[0, -50], [1260, 0]], [[0, 50], [1260, 100]], [[0, 150], [1260, 200]], [[0, 250], [1260, 300]], [[0, 350], [1260, 400]], [[0, 450], [1260, 500]]]
+        },
+        "17": {
+            "numShapes": 1,
+            "layerNumber": 350,
+            "layerDT": 0,
+            "data": [[[450, -59], [810, 509]]]
+        },
+        "18": {
+            "numShapes": 2,
+            "layerNumber": 310,
+            "layerDT": 0,
+            "data": [[[ -30, -75], [30, 525]], [[1230, -75], [1290, 525]]]
+        },
+        "19": {
+            "numShapes": 2,
+            "layerNumber": 601,
+            "layerDT": 0,
+            "data": [[[234, -45], [306, 65]], [[954, -45], [1026, 65]]]
+        }
+    },
+    "numLayers": 20,
+    "cellBbox": [[-30, -75], [1290, 525]]
+};
+
+export const INVD4Data = {
+    "cellName": "INVD4",
+    "layers": {
+        "0": {
+            "numShapes": 7,
+            "layerNumber": 400,
+            "layerDT": 0,
+            "data": [
+                [[234, -55], [306, 75]],
+                [[594, -55], [666, 75]],
+                [[54, -75], [126, 525]],
+                [[414, -75], [486, 525]],
+                [[774, -75], [846, 525]],
+                [[234, 183], [306, 505]],
+                [[594, 183], [666, 505]]
+            ]
+        },
+        "1": {
+            "numShapes": 5,
+            "layerNumber": 402,
+            "layerDT": 0,
+            "data": [
+                [[54, -75], [126, 525]],
+                [[414, -75], [486, 525]],
+                [[774, -75], [846, 525]],
+                [[234, -55], [306, 267]],
+                [[594, -55], [666, 267]]
+            ]
+        },
+        "2": {
+            "numShapes": 4,
+            "layerNumber": 305,
+            "layerDT": 0,
+            "data": [
+                [[150, -59], [210, 509]],
+                [[330, -59], [390, 509]],
+                [[510, -59], [570, 509]],
+                [[690, -59], [750, 509]]
+            ]
+        },
+        "3": {
+            "numShapes": 4,
+            "layerNumber": 307,
+            "layerDT": 0,
+            "data": [
+                [[150, -59], [210, 509]],
+                [[330, -59], [390, 509]],
+                [[510, -59], [570, 509]],
+                [[690, -59], [750, 509]]
+            ]
+        },
+        "4": {
+            "numShapes": 4,
+            "layerNumber": 404,
+            "layerDT": 0,
+            "data": [
+                [[234, -45], [306, 65]],
+                [[594, -45], [666, 65]],
+                [[234, 385], [306, 495]],
+                [[594, 385], [666, 495]]
+            ]
+        },
+        "5": {
+            "numShapes": 4,
+            "layerNumber": 701,
+            "layerDT": 0,
+            "data": [
+                [[140, 441], [220, 509]],
+                [[320, 441], [400, 509]],
+                [[500, 441], [580, 509]],
+                [[680, 441], [760, 509]]
+            ]
+        },
+        "6": {
+            "numShapes": 2,
+            "layerNumber": 1000,
+            "layerDT": 0,
+            "data": [
+                [[44, -49], [856, -1]],
+                [[140, 451], [760, 499]]
+            ]
+        },
+        "7": {
+            "numShapes": 2,
+            "layerNumber": 1000,
+            "layerDT": 1,
+            "data": [
+                ["OUT", 450, -25],
+                ["IN", 450, 475]
+            ]
+        },
+        "8": {
+            "numShapes": 3,
+            "layerNumber": 700,
+            "layerDT": 0,
+            "data": [
+                [[44, -59], [136, 9]],
+                [[404, -59], [496, 9]],
+                [[764, -59], [856, 9]]
+            ]
+        },
+        "9": {
+            "numShapes": 2,
+            "layerNumber": 800,
+            "layerDT": 0,
+            "data": [
+                [[0, -75], [900, 75]],
+                [[0, 375], [900, 525]]
+            ]
+        },
+        "10": {
+            "numShapes": 2,
+            "layerNumber": 800,
+            "layerDT": 1,
+            "data": [
+                ["VSS", 450, 450],
+                ["VDD", 450, 0]
+            ]
+        },
+        "11": {
+            "numShapes": 1,
+            "layerNumber": 103,
+            "layerDT": 0,
+            "data": [
+                [[0, 183], [900, 267]]
+            ]
+        },
+        "12": {
+            "numShapes": 1,
+            "layerNumber": 100,
+            "layerDT": 0,
+            "data": [
+                [[0, 183], [900, 267]]
+            ]
+        },
+        "13": {
+            "numShapes": 3,
+            "layerNumber": 600,
+            "layerDT": 0,
+            "data": [
+                [[54, -16], [126, 66]],
+                [[414, -16], [486, 66]],
+                [[774, -16], [846, 66]]
+            ]
+        },
+        "14": {
+            "numShapes": 1,
+            "layerNumber": 10000,
+            "layerDT": 0,
+            "data": [
+                [[0, -75], [900, 525]]
+            ]
+        },
+        "15": {
+            "numShapes": 6,
+            "layerNumber": 2000,
+            "layerDT": 0,
+            "data": [
+                [[0, -50], [900, 0]],
+                [[0, 50], [900, 100]],
+                [[0, 150], [900, 200]],
+                [[0, 250], [900, 300]],
+                [[0, 350], [900, 400]],
+                [[0, 450], [900, 500]]
+            ]
+        },
+        "16": {
+            "numShapes": 2,
+            "layerNumber": 310,
+            "layerDT": 0,
+            "data": [
+                [[-30, -75], [30, 525]],
+                [[870, -75], [930, 525]]
+            ]
+        },
+        "17": {
+            "numShapes": 2,
+            "layerNumber": 601,
+            "layerDT": 0,
+            "data": [
+                [[234, -45], [306, 65]],
+                [[594, -45], [666, 65]]
+            ]
+        }
+    },
+    "numLayers": 18,
+    "cellBbox": [
+        [-30, -75],
+        [930, 525]
+    ]
+};
+
+export const layerProperties = {
+    800: { color: 0xFD68B3, opacity: 1, offset: 0, height: 5, shape: "box", name: 'bspdn' },
+    404: { color: 0xFF0000, opacity: 1, offset: 4, height: 12, shape: "box", name: 'tsvbar' },
+    400: { color: 0x37FEFD, opacity: 1, offset: 8, height: 6, shape: "box", name: 'nli' },
+    100: { color: 0xFF9966, opacity: 1, offset: 6, height: 10, shape: "diamond", name: 'nmos_ns' },
+    305: { color: 0x30B0B0, opacity: 1, offset: 8, height: 8, shape: "box", name: 'poly_n' },
+    307: { color: 0x46C773, opacity: 1, offset: 16, height: 8, shape: "box", name: 'poly_p' },
+    350: { color: 0xFFD6D6, opacity: 1, offset: 14, height: 4, shape: "box", name: 'diffusion_break' },
+    402: { color: 0x61CB21, opacity: 1, offset: 16, height: 6, shape: "box", name: 'pli' },
+    600: { color: 0x62FF00, opacity: 1, offset: 14, height: 2, shape: "box", name: 'via_li' },
+    103: { color: 0x444444, opacity: 1, offset: 14, height: 10, shape: "diamond", name: 'pmos_ns' },
+    700: { color: 0xFF0080, opacity: 1, offset: 22, height: 10, shape: "box", name: 'vct' },
+    701: { color: 0xFF00FF, opacity: 1, offset: 24, height: 8, shape: "box", name: 'vg' },
+    702: { color: 0xD133FF, opacity: 1, offset: 14, height: 18, shape: "box", name: 'via_nm0' },
+    703: { color: 0x8D0000, opacity: 1, offset: 16, height: 16, shape: "box", name: ''  },
+    1000: { color: 0xF0FF69, opacity: 1, offset: 32, height: 5, shape: "box", name: 'm0' },
+    10000: { color: 0xEFEFEF, opacity: 1, offset: -5, height: 5, shape: "box", name: 'cell_boundary' },
+    // 2000: { color: 0x8F33FF, opacity: 0, offset: 10, height: 10, shape: "box", name: 'routing_tracks' }
+};
---
+++ script.js
@@ -0,0 +1,258 @@
+import * as THREE from 'three';
+import { MUX21X1Data, INVD4Data, layerProperties } from './data.js';
+import { OrbitControls } from "https://unpkg.com/three@0.112/examples/jsm/controls/OrbitControls.js";
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+
+// Defining constants that will be used throughout the script
+const constants = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    offsetScale: 10,
+    highlightColor: 0xFFF000,
+    highlightOpacity: 1
+};
+const cellData = MUX21X1Data;
+
+// Initializing certain neccessary items
+const scene = new THREE.Scene();
+const clock = new THREE.Clock();
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+const camera = new THREE.PerspectiveCamera(75, constants.width / constants.height);
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+const frontLight = new THREE.DirectionalLight(0xffffff, 1);
+const backLight = new THREE.DirectionalLight(0xffffff, 1);
+const lightTargetObject = new THREE.Object3D();
+
+let highlightedObjects = []; // This will store the objects that are highlighted once clicked
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Camera is positioned at the center of the cell bounding box, 1000 units directly above
+    camera.position.x = (cellData.cellBbox[0][0] + cellData.cellBbox[1][0]) / 2;
+    camera.position.y = (cellData.cellBbox[0][1] + cellData.cellBbox[1][1]) / 2;
+    camera.position.z = 1000;
+    scene.add(camera);
+
+    // Lighting at the camera's position and behind the object
+    scene.add(ambientLight);
+    lightTargetObject.position.set(camera.position.x, camera.position.y, 0);
+    scene.add(lightTargetObject);
+    frontLight.position.set(camera.position.x, camera.position.y, camera.position.z);
+    frontLight.target = lightTargetObject;
+    scene.add(frontLight);
+    backLight.position.set(camera.position.x, camera.position.y, -camera.position.z);
+    backLight.target = lightTargetObject;
+    scene.add(backLight);
+
+    // Add the shapes that we create from each layer to the scene
+    addShapes(cellData);
+
+    // Render everything on the canvas
+    const canvas = document.querySelector('canvas.webgl');
+    const renderer = new THREE.WebGLRenderer({
+        canvas: canvas
+    });
+    renderer.setSize(constants.width, constants.height);
+    renderer.setAnimationLoop( render );
+    const cameraControls = new OrbitControls(camera, renderer.domElement);
+    cameraControls.target.set((cellData.cellBbox[0][0] + cellData.cellBbox[1][0]) / 2, (cellData.cellBbox[0][1] + cellData.cellBbox[1][1]) / 2, 0);
+    window.addEventListener('click', highlightObjects, false);
+    animate();
+
+    // Helper functions for the animation
+    function animate() {
+        requestAnimationFrame(animate);
+        render();
+    }
+    function render() {
+        var delta = clock.getDelta();
+        cameraControls.update(delta);
+        renderer.render(scene, camera);
+    }
+});
+
+const createBox = (coords, heightPos, layerProperties) => {
+    const [start, end] = coords;
+    const geometry = new THREE.BoxGeometry(end[0] - start[0], end[1] - start[1], heightPos[1] - heightPos[0]);
+    const material = new THREE.MeshStandardMaterial({color: layerProperties.color, transparent: true, opacity: layerProperties.opacity, metalness: 0, roughness: 1});
+    const box = new THREE.Mesh(geometry, material);
+    box.position.set(
+        (start[0] + end[0]) / 2,
+        (start[1] + end[1]) / 2,
+        (heightPos[0] + heightPos[1]) / 2
+    );
+    scene.add(box);
+};
+
+const createCylinder = (coords, heightPos, layerProperties) => {
+    const [start, end] = coords;
+    const r = 20;
+    const geometry = new THREE.CylinderGeometry(r, r, end[0] - start[0], 24); 
+    const material = new THREE.MeshBasicMaterial({color: layerProperties.color, transparent: true, opacity: layerProperties.opacity}); 
+    const cylinder = new THREE.Mesh(geometry, material);
+    cylinder.rotation.z = Math.PI / 2;
+    cylinder.position.set(
+        (start[0] + end[0]) / 2,
+        (start[1] + end[1]) / 2,
+        (heightPos[0] + heightPos[1]) / 2
+    );
+    scene.add(cylinder);
+};
+
+const createDiamond = (coords, heightPos, layerProperties) => {
+    const [start, end] = coords;
+    const r = 20;
+    const geometry = new THREE.CylinderGeometry(r, r, end[0] - start[0], 4);
+    const material = new THREE.MeshStandardMaterial({color: layerProperties.color, transparent: true, opacity: layerProperties.opacity, metalness: 0, roughness: 1}); 
+    const diamond = new THREE.Mesh(geometry, material);
+    diamond.rotation.z = Math.PI / 2;
+    diamond.position.set(
+        (start[0] + end[0]) / 2,
+        (start[1] + end[1]) / 2,
+        (heightPos[0] + heightPos[1]) / 2
+    );
+    scene.add(diamond);
+};
+
+const createText = (textData, heightOffset) => {
+    const loader = new FontLoader();
+    const [text, x, y] = textData;
+    loader.load( 'https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function ( font ) {
+        const textGeometry = new TextGeometry(text, {
+            font: font,
+            size: 20,
+            depth: 1
+        });
+        const textMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+        const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+        textMesh.position.set(x, y, heightOffset);
+        scene.add(textMesh);
+    });
+};
+
+const addShapes = (cellData) => {
+    const layers = cellData.layers;
+    for (let layerKey in layers) {
+        const layer = layers[layerKey];
+        if (layer.layerNumber in layerProperties) {
+            const layerProps = layerProperties[layer.layerNumber]
+            let offsetStart = constants.offsetScale * layerProps.offset;
+            let offsetEnd = constants.offsetScale * (layerProps.offset + layerProps.height);
+            const heightPos = [offsetStart, offsetEnd];
+            if (layer.layerDT === 1) {
+                layer.data.forEach(textData => {
+                    createText(textData, offsetEnd);
+                });
+            }
+            else if (layerProps.shape === 'box') {
+                layer.data.forEach(coords => {
+                    createBox(coords, heightPos, layerProps);
+                });
+            }
+            else if (layerProps.shape === 'diamond') {
+                layer.data.forEach(coords => {
+                    createDiamond(coords, heightPos, layerProps);
+                });
+            }
+            else if (layerProps.shape === 'cylinder') {
+                layer.data.forEach(coords => {
+                    createCylinder(coords, heightPos, layerProps);
+                });
+            }
+        } else {
+            console.log(`Layer ${layer.layerNumber} is ignored`);
+        }
+    }
+};
+
+function getObjectsTouching(selectedObject) {
+    // Create a bounding box for the selected object
+    const selectedBox = new THREE.Box3().setFromObject(selectedObject);
+
+    // Array to store objects that are touching the selected object
+    const touchingObjects = [selectedObject];
+
+    // Iterate through all objects in the scene
+    scene.children.forEach(obj => {
+        if (obj !== selectedObject && obj.isMesh) {
+            // Create a bounding box for the current object
+            const objectBox = new THREE.Box3().setFromObject(obj);
+
+            // Check if the bounding boxes intersect
+            if (selectedBox.intersectsBox(objectBox)) {
+                touchingObjects.push(obj);
+            }
+        }
+    });
+
+    return touchingObjects;
+}
+
+function getObjectsTouchingRecursive(selectedObject, checkedObjects = new Set()) {
+    // Create a bounding box for the selected object
+    const selectedBox = new THREE.Box3().setFromObject(selectedObject);
+
+    // Array to store objects that are touching the selected object
+    const touchingObjects = [selectedObject];
+
+    // Mark the selected object as checked
+    checkedObjects.add(selectedObject);
+
+    // Iterate through all objects in the scene
+    scene.children.forEach(obj => {
+        if (obj !== selectedObject && obj.isMesh && !checkedObjects.has(obj)) {
+            // Create a bounding box for the current object
+            const objectBox = new THREE.Box3().setFromObject(obj);
+
+            // Check if the bounding boxes intersect
+            if (selectedBox.intersectsBox(objectBox)) {
+                touchingObjects.push(obj);
+                checkedObjects.add(obj); // Mark this object as checked
+            }
+        }
+    });
+
+    // Recursively check for objects touching the newly found objects
+    touchingObjects.forEach(obj => {
+        const moreTouchingObjects = getObjectsTouching(obj, checkedObjects);
+        moreTouchingObjects.forEach(touchingObj => {
+            if (!touchingObjects.includes(touchingObj)) {
+                touchingObjects.push(touchingObj);
+            }
+        });
+    });
+
+    return touchingObjects;
+}
+
+function highlightObjects(event) {
+    // Calculate mouse position in normalized device coordinates (-1 to +1) for both components.
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    // Calculate objects intersecting the ray.
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObjects(scene.children, true);
+    // Reset the opacity of all objects in the scene
+    scene.children.forEach(obj => {
+        if (obj.isMesh) { // Check if the object is a mesh
+            if (obj.originalOpacity !== undefined) {
+                obj.material.opacity = obj.originalOpacity;
+            } else {
+                obj.originalOpacity = obj.material.opacity;
+            }
+        }
+    });
+    if (intersects.length > 0) {
+        const touchingObjects = getObjectsTouchingRecursive(intersects[0].object);
+        // Collect all intersected objects
+        // const intersectedObjects = intersects.map(intersect => intersect.object);
+        console.log(`There are ${touchingObjects.length} intersected objects`, touchingObjects);
+        // Set the opacity of non-intersected objects to 0.2
+        scene.children.forEach(obj => {
+            if (obj.isMesh && !touchingObjects.includes(obj)) {
+                obj.material.opacity = 0.1;
+            }
+        });
+    }
+}
