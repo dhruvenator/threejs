dc49a515ccc802afe643076ee694cf03e60d84cf
dhruvenator <dhruva@siclarity.com>
2025-03-05 11:10:05 -0800

    some cleaning up

--- script.js
+++ script.js
@@ -2,12 +2,9 @@ import * as THREE from 'three';
 import { OrbitControls } from "https://unpkg.com/three@0.112/examples/jsm/controls/OrbitControls.js";
 import { FontLoader } from 'https://cdn.jsdelivr.net/npm/three@0.174.0/examples/jsm/loaders/FontLoader.js';
 import { TextGeometry } from 'https://cdn.jsdelivr.net/npm/three@0.174.0/examples/jsm/geometries/TextGeometry.js';
-//import { OBJExporter } from 'https://cdn.jsdelivr.net/npm/three@0.174.0/addons/exporters/OBJExporter.js';
 import dat from "https://cdn.skypack.dev/dat.gui";
 import { RoundedBoxGeometry } from 'https://cdn.jsdelivr.net/npm/three@0.174.0/examples/jsm/Addons.js';
 import { MUXNEW, layerProperties } from './data.js';
-
-// import NewMux from "/Users/dhruvix/Documents/twod/mux_json_5/MUX21X1_1_RT_5_360.json" assert {type: "json"};
 
 // Defining constants that will be used throughout the script
 const constants = {
@@ -15,13 +12,15 @@ const constants = {
     height: window.innerHeight,
     offsetScale: 1,
     greyColor: 0xDEDEDE,
-    highlightOpacity: 1
+    highlightOpacity: 1,
+    segments: 10,
+    rounding: 7,
+    nanoNum: 1
 };
 const cellData = MUXNEW;
 
 // Initializing certain neccessary items
 const scene = new THREE.Scene();
-const clock = new THREE.Clock();
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 const camera = new THREE.PerspectiveCamera(75, constants.width / constants.height, 1, 5000);
@@ -29,109 +28,45 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 const frontLight = new THREE.DirectionalLight(0xffffff, 1);
 const backLight = new THREE.DirectionalLight(0xffffff, 1);
 const lightTargetObject = new THREE.Object3D();
-//const exporter = new OBJExporter();
+const gui = new dat.GUI();
+let renderer = new THREE.WebGLRenderer();
 
+// Clipping planes for the renderer
 const clippingPlaneX = new THREE.Plane(new THREE.Vector3(1, 0, 0), -cellData.cellBbox[0][0] + 5);
-// Create clipping planes
-const clippingPlaneY = new THREE.Plane(new THREE.Vector3(0, 1, 0), -cellData.cellBbox[0][1]+5);  // Y-axis clipping
-const clippingPlaneZ = new THREE.Plane(new THREE.Vector3(0, 0, 1), 500);  // Z-axis clipping
-const gui=new dat.GUI();
-gui.add(clippingPlaneX, 'constant', -cellData.cellBbox[1][0]-5 , -cellData.cellBbox[0][0]+5).name('X Clipping Position').onChange(updateClippingPlanes);
-gui.add(clippingPlaneY, 'constant', -cellData.cellBbox[1][1]-5 , -cellData.cellBbox[0][1]+5).name('Y Clipping Position').onChange(updateClippingPlanes);
+const clippingPlaneY = new THREE.Plane(new THREE.Vector3(0, 1, 0), -cellData.cellBbox[0][1] + 5);
+const clippingPlaneZ = new THREE.Plane(new THREE.Vector3(0, 0, 1), 500);
+gui.add(clippingPlaneX, 'constant', -cellData.cellBbox[1][0] - 5, -cellData.cellBbox[0][0] + 5).name('X Clipping Position').onChange(updateClippingPlanes);
+gui.add(clippingPlaneY, 'constant', -cellData.cellBbox[1][1] - 5, -cellData.cellBbox[0][1] + 5).name('Y Clipping Position').onChange(updateClippingPlanes);
 gui.add(clippingPlaneZ, 'constant', -500, 500).name('Z Clipping Position').onChange(updateClippingPlanes);
 
-let segments=10;
-let rounding=7;
-let nano_num=1;
-const nano_param={
-    Sheets:'1'
-}
-const rounding_param = {
-    Roundness: "7"
-}
-const segment_param={
-    Segments:"10"
-}
+// Arrays to store the objects created in the scene
+let sheets = [];
+let boxes = [];
+let diamonds = [];
+let cylinders = [];
+let textMeshes = [];
 
-gui.add(rounding_param, "Roundness").onChange(function (value) {
-    rounding=value;
-    clearGeometry();
-    addShapes(cellData);
-});
-gui.add(segment_param,"Segments").onChange(function (value) {
-    segments=value;
-    clearGeometry();
-    addShapes(cellData);
-});
-gui.add(nano_param,"Sheets").onChange(function (value) {
-    nano_num=value;
-    //console.log(nano_num);
-    clearGeometry();
-    addShapes(cellData);
-});
-let sheets=[];
-let boxes=[];
-let diamonds=[];
-let cylinders=[];
-let textMeshes=[];
-// Function to clear all existing sheets from the scene
-function clearGeometry() {
-    // Remove all sheets from the scene
-    sheets.forEach(sheet => {
-        scene.remove(sheet);
-    });
-    sheets = []; 
-    boxes.forEach(box => {
-        scene.remove(box);
-    });
-    boxes=[];
-    diamonds.forEach(diamond => {
-        scene.remove(diamond);
-    });
-    diamonds=[];
-    cylinders.forEach(cylinder => {
-        scene.remove(cylinder);
-    });
-    cylinders=[];
-    console.log('Removed Geometries');
-    textMeshes.forEach((textMesh) => {
-        scene.remove(textMesh);
-    });
-    textMeshes.length = 0;  // Clear the array
-    console.log('Removed Text');
+// GUI parameters
+const nanoSheetsParam = {
+    Sheets: constants.nanoNum.toString()
 }
-const f1 = gui.addFolder('Layers');
-const f2 = gui.addFolder('Conducting Paths');
+const roundingParam = {
+    Roundness: constants.rounding.toString()
+}
+gui.add(roundingParam, "Roundness").onChange(value => {
+    constants.rounding = value;
+    clearGeometry();
+    addShapes(cellData);
+});
+gui.add(nanoSheetsParam,"Sheets").onChange(value => {
+    constants.nanoNum = value;
+    clearGeometry();
+    addShapes(cellData);
+});
+const layerFolder = gui.addFolder('Layers');
+const conductingFolder = gui.addFolder('Conducting Paths');
 const layerDictionary = {};
 
-// Function to add values to a specific layer
-function addValueToLayer(layerNumber, value) {
-    if (!layerDictionary[layerNumber]) {
-        layerDictionary[layerNumber] = []; // Initialize the layer's array if it doesn't exist
-    }
-    layerDictionary[layerNumber].push(value);
-}
-
-function updateClippingPlanes() {
-    const planes = [clippingPlaneX, clippingPlaneY, clippingPlaneZ];
-    // Update renderer's clipping planes
-    renderer.clippingPlanes = planes;
-    renderer.localClippingEnabled = true;
-}
-
-const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('canvas.webgl') });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(render);
-
-function render() {
-    renderer.render(scene, camera);
-}
-function controlLayerVisibility(layerName, showMesh) {
-    layerDictionary[layerName].forEach(shape => {
-            shape.visible = showMesh;
-        });
-};
-updateClippingPlanes();
 document.addEventListener('DOMContentLoaded', () => {
     // Camera is positioned at the center of the cell bounding box, 1000 units directly above
     camera.position.x = (cellData.cellBbox[0][0] + cellData.cellBbox[1][0]) / 2;
@@ -152,91 +87,91 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Add the shapes that we create from each layer to the scene
     addShapes(cellData);
-    console.log(layerDictionary);
-    const layerNames =  Object.keys(layerDictionary);
-    console.log('Layer Names:', layerNames);
-    layerNames.forEach(layerName => {
-        console.log(`Layer Adding ${layerName}:`);
-        const layerVariables = {
-            showMesh: true
-        };
-        f1.add(layerVariables, 'showMesh').name(layerName).onChange(function(value){
-            console.log(value);
-            console.log(layerName);
-            controlLayerVisibility(layerName, value);
-        })
-    });
+    populateGUILayers();
+    
     // Render everything on the canvas
-    const canvas = document.querySelector('canvas.webgl');
-    // const renderer = new THREE.WebGLRenderer({
-    //     canvas: canvas
-    // });
-    // renderer.setSize(constants.width, constants.height);
-    // renderer.setAnimationLoop(render);
+    renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('canvas.webgl') });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    updateClippingPlanes();
+
     const cameraControls = new OrbitControls(camera, renderer.domElement);
     cameraControls.target.set((cellData.cellBbox[0][0] + cellData.cellBbox[1][0]) / 2, (cellData.cellBbox[0][1] + cellData.cellBbox[1][1]) / 2, 0);
     window.addEventListener('click', highlightObjects, false);
-    // // Enable clipping globally
-    // renderer.clippingPlanes = [clippingPlane,clippingPlaneX];
-    // renderer.localClippingEnabled = true;
 
     animate();
-
-    // Helper functions for the animation
-    function animate() {
-        requestAnimationFrame(animate);
-        // Move the clipping plane dynamically along the Y-axis
-        // if (clippingPlane.constant > 2) clippingPlane.constant = -5; 
-        render();
-    }
-    // f1 = gui.addFolder('Layers');
-
-    
-
-
 });
 
-function createCheckboxesFromDict(dict, divId, callback) {
-    const container = document.getElementById(divId);
-    if (!container) {
-        console.error(`Div with ID "${divId}" not found.`);
-        return;
-    }
-
-    for (const key in dict) {
-        if (dict.hasOwnProperty(key)) {
-            const checkbox = document.createElement('input');
-            checkbox.type = 'checkbox';
-            checkbox.id = key;
-            checkbox.value = key;
-
-            const label = document.createElement('label');
-            label.htmlFor = key;
-            label.textContent = key;
-
-            checkbox.addEventListener('change', function () {
-                callback(key, this.checked);
+//Function to add layer names to the GUI layers folder
+function populateGUILayers() {
+    const layerNames =  Object.keys(layerDictionary);
+    layerNames.forEach(layerName => {
+        console.log(`Adding layer ${layerName} to GUI`);
+        const layerVariables = {
+            showMesh: true
+        };
+        layerFolder.add(layerVariables, 'showMesh').name(layerName).onChange(value => {
+            layerDictionary[layerName].forEach(shape => {
+                shape.visible = value;
             });
-
-            container.appendChild(checkbox);
-            container.appendChild(label);
-            container.appendChild(document.createElement('br'));
-        }
-    }
+        })
+    });
 }
 
-function handleCheckboxChange(key, isChecked) {
-    console.log(`Checkbox with key "${key}" changed. Checked: ${isChecked}`);
+// Function to add values to a specific layer
+function addValueToLayer(layerNumber, value) {
+    if (!layerDictionary[layerNumber]) {
+        layerDictionary[layerNumber] = []; // Initialize the layer's array if it doesn't exist
+    }
+    layerDictionary[layerNumber].push(value);
+}
+
+function updateClippingPlanes() {
+    const planes = [clippingPlaneX, clippingPlaneY, clippingPlaneZ];
+    renderer.clippingPlanes = planes;
+    renderer.localClippingEnabled = true;
+}
+
+// Helper functions for the animation
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+// Function to remove all objects from the scene
+function clearGeometry() {
+    sheets.forEach(sheet => {
+        scene.remove(sheet);
+    });
+    sheets = []; 
+    boxes.forEach(box => {
+        scene.remove(box);
+    });
+    boxes = [];
+    diamonds.forEach(diamond => {
+        scene.remove(diamond);
+    });
+    diamonds = [];
+    cylinders.forEach(cylinder => {
+        scene.remove(cylinder);
+    });
+    cylinders = [];
+    textMeshes.forEach((textMesh) => {
+        scene.remove(textMesh);
+    });
+    textMeshes = [];
+    console.log('Removed Geometries');
 }
 
 // Function to handle multiple sheets
 const createSheets = (coords, heightPos, layerProperties, numSheets) => {
     const [start, end] = coords;
     const distance = 1.5 * (heightPos[1] - heightPos[0]);
+    const nanoOffset = 0; // TODO: Change this to center nanosheets
     // Loop through to create 'numSheets' sheets
     for (let i = 0; i < numSheets; i++) {
         // Create the geometry for the sheet
-        const geometry = new RoundedBoxGeometry(end[0] - start[0], end[1] - start[1], heightPos[1] - heightPos[0], segments, rounding);
+        const geometry = new RoundedBoxGeometry(end[0] - start[0], end[1] - start[1], heightPos[1] - heightPos[0], constants.segments, constants.rounding);
         // Material settings
         const material = new THREE.MeshStandardMaterial({ color: layerProperties.color, transparent: true, opacity: layerProperties.opacity, metalness: 0, roughness: 1 });
         // Create the mesh and set the position of the sheet
@@ -244,18 +179,17 @@ const createSheets = (coords, heightPos, layerProperties, numSheets) => {
         nano.position.set(
             (start[0] + end[0]) / 2,
             (start[1] + end[1]) / 2,
-            (heightPos[0] + heightPos[1]) / 2 + (i * distance)
+            (heightPos[0] + heightPos[1]) / 2 + (i * distance) + nanoOffset
         );
-
         // Add to the scene and store the object in the array for future manipulation
         scene.add(nano);
         sheets.push(nano);
     }
 };
 
-const createBox = (coords, heightPos, layerProperties, numSegments, numRounding) => {
+const createBox = (coords, heightPos, layerProperties, numRounding) => {
     const [start, end] = coords;
-    const geometry = new RoundedBoxGeometry(end[0] - start[0], end[1] - start[1], heightPos[1] - heightPos[0], numSegments, numRounding);
+    const geometry = new RoundedBoxGeometry(end[0] - start[0], end[1] - start[1], heightPos[1] - heightPos[0], constants.segments, numRounding);
     //const geometry = new THREE.BoxGeometry(end[0] - start[0], end[1] - start[1], heightPos[1] - heightPos[0]);
     const material = new THREE.MeshStandardMaterial({ color: layerProperties.color, transparent: true, opacity: layerProperties.opacity, metalness: 0, roughness: 1 });
     const box = new THREE.Mesh(geometry, material);
@@ -265,7 +199,6 @@ const createBox = (coords, heightPos, layerProperties, numSegments, numRounding)
         (heightPos[0] + heightPos[1]) / 2
     );
     scene.add(box);
-    // console.log(layerProperties);
     addValueToLayer(layerProperties.name, box);
     boxes.push(box);
 };
@@ -320,9 +253,8 @@ const createText = (textData, heightOffset) => {
 };
 
 const addShapes = (cellData) => {
-    let numSheets = nano_num;
-    let numSegments = segments;
-    let numRounding = rounding;
+    let numSheets = constants.nanoNum;
+    let numRounding = constants.rounding;
     const layers = cellData.layers;
     for (let layerKey in layers) {
         const layer = layers[layerKey];
@@ -338,7 +270,7 @@ const addShapes = (cellData) => {
             }
             else if (layerProps.shape === 'box') {
                 layer.data.forEach(coords => {
-                    createBox(coords, heightPos, layerProps, numSegments, numRounding);
+                    createBox(coords, heightPos, layerProps, numRounding);
                 });
             }
             else if (layerProps.shape === 'diamond') {
@@ -418,7 +350,6 @@ function highlightObjects(event) {
         });
     }
 }
-
 
 function saveFile(text, filename) {
     const blob = new Blob([text], { type: 'text/plain' });
