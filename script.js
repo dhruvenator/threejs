7b80814ae9834f424b6b490922acaa97f5b0bbc8
dhruvenator <dhruva@siclarity.com>
2025-03-14 13:34:45 -0700

    added clipping planes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { OrbitControls } from "https://unpkg.com/three@0.112/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { FontLoader } from 'https://cdn.jsdelivr.net/npm/three@0.174.0/examples/jsm/loaders/FontLoader.js';
 import { TextGeometry } from 'https://cdn.jsdelivr.net/npm/three@0.174.0/examples/jsm/geometries/TextGeometry.js';
 import dat from "https://cdn.skypack.dev/dat.gui";
@@ -12,6 +12,7 @@ const constants = {
     height: window.innerHeight,
     offsetScale: 1,
     greyColor: 0xDEDEDE,
+    clippingPlaneColor: 0xE91E63,
     highlightOpacity: 1,
     segments: 10,
     rounding: 7,
@@ -29,6 +30,7 @@ const frontLight = new THREE.DirectionalLight(0xffffff, 1);
 const backLight = new THREE.DirectionalLight(0xffffff, 1);
 const lightTargetObject = new THREE.Object3D();
 const gui = new dat.GUI();
+const geomGroup = new THREE.Group();
 let renderer = new THREE.WebGLRenderer();
 
 // Arrays to store the objects created in the scene
@@ -48,14 +50,36 @@ Object.keys(layerProperties).forEach(key => {
 });
 console.log()
 
+// parameters to be used in the gui for the clipping planes
+const params = {
+    planeX: {
+        constant: cellData.cellBbox[1][0] + 5,
+        negated: false,
+        displayHelper: false
+    },
+    planeY: {
+        constant: cellData.cellBbox[1][1] + 5,
+        negated: false,
+        displayHelper: false
+    },
+    planeZ: {
+        constant: z_end + 5,
+        negated: false,
+        displayHelper: false
+    }
+};
+
 // Clipping planes for the renderer
-const clippingPlaneX = new THREE.Plane(new THREE.Vector3(1, 0, 0), -cellData.cellBbox[0][0] + 5);
-const clippingPlaneY = new THREE.Plane(new THREE.Vector3(0, 1, 0), -cellData.cellBbox[0][1] + 5);
-const clippingPlaneZ = new THREE.Plane(new THREE.Vector3(0, 0, 1), -z_start);
+const clippingPlaneX = new THREE.Plane(new THREE.Vector3(-1, 0, 0), cellData.cellBbox[1][0] + 5);
+const clippingPlaneY = new THREE.Plane(new THREE.Vector3(0, -1, 0), cellData.cellBbox[1][1] + 5);
+const clippingPlaneZ = new THREE.Plane(new THREE.Vector3(0, 0, -1), z_end + 5);
 const clippingPlanes = [clippingPlaneX, clippingPlaneY, clippingPlaneZ];
-gui.add(clippingPlaneX, 'constant', -cellData.cellBbox[1][0] - 5, -cellData.cellBbox[0][0] + 5).name('X Clipping Position').onChange(updateClippingPlanes);
-gui.add(clippingPlaneY, 'constant', -cellData.cellBbox[1][1] - 5, -cellData.cellBbox[0][1] + 5).name('Y Clipping Position').onChange(updateClippingPlanes);
-gui.add(clippingPlaneZ, 'constant', -z_end - 5, -z_start + 5).name('Z Clipping Position').onChange(updateClippingPlanes);
+const planeHelpers = clippingPlanes.map( p => new THREE.PlaneHelper( p, 4000, 0x000000 ) );
+planeHelpers.forEach( ph => {
+    ph.visible = false;
+    scene.add( ph );
+});
+const planeObjects = [];
 
 // GUI parameters
 const nanoSheetsParam = {
@@ -101,13 +125,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Add the shapes that we create from each layer to the scene
     addShapes(cellData);
+    setUpClippingPlanes();
     populateGUILayers();
     
     // Render everything on the canvas
     renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('canvas.webgl'), antialias: true, stencil: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
-
-    updateClippingPlanes();
+    renderer.localClippingEnabled = true;
 
     const cameraControls = new OrbitControls(camera, renderer.domElement);
     cameraControls.target.set((cellData.cellBbox[0][0] + cellData.cellBbox[1][0]) / 2, (cellData.cellBbox[0][1] + cellData.cellBbox[1][1]) / 2, (z_start + z_end) / 2);
@@ -119,11 +143,141 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function animate() {
         requestAnimationFrame(animate);
+        for ( let i = 0; i < planeObjects.length; i ++ ) {
+            const plane = clippingPlanes[ i ];
+            const po = planeObjects[ i ];
+            plane.coplanarPoint( po.position );
+            po.lookAt(
+                po.position.x - plane.normal.x,
+                po.position.y - plane.normal.y,
+                po.position.z - plane.normal.z,
+            );
+        }
         cameraControls.update();
         renderer.render(scene, camera);
     }
     
 });
+
+// function to render the clipping planes
+function createPlaneStencilGroup( geometry, plane, renderOrder ) {
+    const group = new THREE.Group();
+    const baseMat = new THREE.MeshBasicMaterial();
+    baseMat.depthWrite = false;
+    baseMat.depthTest = false;
+    baseMat.colorWrite = false;
+    baseMat.stencilWrite = true;
+    baseMat.stencilFunc = THREE.AlwaysStencilFunc;
+    // back faces
+    const mat0 = baseMat.clone();
+    mat0.side = THREE.BackSide;
+    mat0.clippingPlanes = [ plane ];
+    mat0.stencilFail = THREE.IncrementWrapStencilOp;
+    mat0.stencilZFail = THREE.IncrementWrapStencilOp;
+    mat0.stencilZPass = THREE.IncrementWrapStencilOp;
+    for ( let i = 0; i < geometry.length; i ++ ) {
+        const mesh0 = new THREE.Mesh( geometry[i], mat0 );
+        mesh0.renderOrder = renderOrder;
+        group.add( mesh0 );
+    }
+    // front faces
+    const mat1 = baseMat.clone();
+    mat1.side = THREE.FrontSide;
+    mat1.clippingPlanes = [ plane ];
+    mat1.stencilFail = THREE.DecrementWrapStencilOp;
+    mat1.stencilZFail = THREE.DecrementWrapStencilOp;
+    mat1.stencilZPass = THREE.DecrementWrapStencilOp;
+    for ( let i = 0; i < geometry.length; i ++ ) {
+        const mesh1 = new THREE.Mesh( geometry[i], mat1 );
+        mesh1.renderOrder = renderOrder;
+        group.add( mesh1 );
+    }
+    return group;
+}
+
+function setUpClippingPlanes() {
+    let objectGroup = new THREE.Group();
+    scene.add( objectGroup );
+    // Set up clip plane rendering
+    const planeGeom = new THREE.PlaneGeometry( 4000, 4000 );
+    for ( let i = 0; i < 3; i ++ ) {
+        const poGroup = new THREE.Group();
+        const plane = clippingPlanes[i];
+        const stencilGroup = createPlaneStencilGroup( geomGroup, plane, i + 1 );
+        // plane is clipped by the other clipping planes
+        const planeMat = new THREE.MeshStandardMaterial({
+            color: constants.clippingPlaneColor,
+            metalness: 0.1,
+            roughness: 0.75,
+            clippingPlanes: clippingPlanes.filter( p => p !== plane ),
+            stencilWrite: true,
+            stencilRef: 0,
+            stencilFunc: THREE.NotEqualStencilFunc,
+            stencilFail: THREE.ReplaceStencilOp,
+            stencilZFail: THREE.ReplaceStencilOp,
+            stencilZPass: THREE.ReplaceStencilOp,
+        });
+        const po = new THREE.Mesh( planeGeom, planeMat );
+        po.onAfterRender = function ( renderer ) {
+            renderer.clearStencil();
+        };
+        po.renderOrder = i + 1.1;
+        objectGroup.add( stencilGroup );
+        poGroup.add( po );
+        planeObjects.push( po );
+        scene.add( poGroup );
+    }
+    // Add functionalities for X clipping plane
+    const planeX = gui.addFolder( 'X clipping plane' );
+    planeX.add( params.planeX, 'displayHelper' ).onChange( v => planeHelpers[0].visible = v );
+    planeX.add( params.planeX, 'constant' ).min(cellData.cellBbox[0][0] - 5 ).max(cellData.cellBbox[1][0] + 5).onChange( d => {
+        if (params.planeX.negated){
+            clippingPlanes[0].constant = -d;
+        }
+        else{
+            clippingPlanes[0].constant = d;
+        }
+    });
+    planeX.add( params.planeX, 'negated' ).onChange( () => {
+        clippingPlanes[0].negate();
+        params.planeX.constant = clippingPlanes[0].constant;
+    } );
+    planeX.open();
+    // Add functionalities for Y clipping plane
+    const planeY = gui.addFolder( 'Y clipping plane' );
+    planeY.add( params.planeY, 'displayHelper' ).onChange( v => planeHelpers[1].visible = v );
+    planeY.add( params.planeY, 'constant' ).min(cellData.cellBbox[0][1] - 5 ).max( cellData.cellBbox[1][1] + 5 ).onChange( d =>{
+        if (params.planeY.negated){
+            clippingPlanes[1].constant = -d;
+        }
+        else{
+            clippingPlanes[1].constant = d;
+        }
+    });
+    planeY.add( params.planeY, 'negated' ).onChange( () => {
+        clippingPlanes[1].negate();
+        params.planeY.constant = clippingPlanes[1].constant;
+    } );
+    planeY.open();
+    // Add functionalities for Z clipping plane
+    const planeZ = gui.addFolder( 'Z clipping plane' );
+    planeZ.add( params.planeZ, 'displayHelper' ).onChange( v => planeHelpers[2].visible = v );
+    planeZ.add( params.planeZ, 'constant' ).min( z_start - 5 ).max( z_end + 5 ).onChange( d => {
+        if (params.planeZ.negated){
+            clippingPlanes[2].constant = -d;
+        }
+        else{
+            clippingPlanes[2].constant = d
+        }
+    });
+    planeZ.add( params.planeZ, 'negated' ).onChange( () => {
+        clippingPlanes[2].negate();
+        params.planeZ.constant = clippingPlanes[2].constant;
+    } );
+    planeZ.open();
+
+    scene.add(geomGroup);
+}
 
 //Function to add layer names to the GUI layers folder
 function populateGUILayers() {
@@ -148,13 +302,6 @@ function addValueToLayer(layerNumber, value) {
     }
     layerDictionary[layerNumber].push(value);
 }
-
-function updateClippingPlanes() {
-    renderer.clippingPlanes = clippingPlanes;
-    renderer.localClippingEnabled = true;
-}
-
-// Helper functions for the animation
 
 // Function to remove all objects from the scene
 function clearGeometry() {
@@ -191,7 +338,7 @@ const createSheets = (coords, heightPos, layerProperties, numSheets) => {
         // Create the geometry for the sheet
         const geometry = new RoundedBoxGeometry(end[0] - start[0], end[1] - start[1], heightPos[1] - heightPos[0], constants.segments, constants.rounding);
         // Material settings
-        const material = new THREE.MeshStandardMaterial({ color: layerProperties.color, transparent: true, opacity: layerProperties.opacity, metalness: 0, roughness: 1 });
+        const material = new THREE.MeshStandardMaterial({ color: layerProperties.color, transparent: true, opacity: layerProperties.opacity, metalness: 0, roughness: 1, clippingPlanes: clippingPlanes });
         // Create the mesh and set the position of the sheet
         const nano = new THREE.Mesh(geometry, material);
         nano.position.set(
@@ -209,7 +356,7 @@ const createBox = (coords, heightPos, layerProperties, numRounding) => {
     const [start, end] = coords;
     const geometry = new RoundedBoxGeometry(end[0] - start[0], end[1] - start[1], heightPos[1] - heightPos[0], constants.segments, numRounding);
     //const geometry = new THREE.BoxGeometry(end[0] - start[0], end[1] - start[1], heightPos[1] - heightPos[0]);
-    const material = new THREE.MeshStandardMaterial({ color: layerProperties.color, transparent: true, opacity: layerProperties.opacity, metalness: 0, roughness: 1 });
+    const material = new THREE.MeshStandardMaterial({ color: layerProperties.color, transparent: true, opacity: layerProperties.opacity, metalness: 0, roughness: 1, clippingPlanes: clippingPlanes });
     const box = new THREE.Mesh(geometry, material);
     box.position.set(
         (start[0] + end[0]) / 2,
@@ -225,7 +372,7 @@ const createCylinder = (coords, heightPos, layerProperties) => {
     const [start, end] = coords;
     const r = 20;
     const geometry = new THREE.CylinderGeometry(r, r, end[0] - start[0], 24);
-    const material = new THREE.MeshBasicMaterial({ color: layerProperties.color, transparent: true, opacity: layerProperties.opacity });
+    const material = new THREE.MeshBasicMaterial({ color: layerProperties.color, transparent: true, opacity: layerProperties.opacity, clippingPlanes: clippingPlanes });
     const cylinder = new THREE.Mesh(geometry, material);
     cylinder.rotation.z = Math.PI / 2;
     cylinder.position.set(
@@ -241,7 +388,7 @@ const createDiamond = (coords, heightPos, layerProperties) => {
     const [start, end] = coords;
     const r = (heightPos[1] - heightPos[0]) / 2;
     const geometry = new THREE.CylinderGeometry(r, r, end[0] - start[0], 4);
-    const material = new THREE.MeshStandardMaterial({ color: layerProperties.color, transparent: true, opacity: layerProperties.opacity, metalness: 0, roughness: 1 });
+    const material = new THREE.MeshStandardMaterial({ color: layerProperties.color, transparent: true, opacity: layerProperties.opacity, metalness: 0, roughness: 1, clippingPlanes: clippingPlanes, polygonOffset: true, polygonOffsetFactor: -0.75 });
     const diamond = new THREE.Mesh(geometry, material);
     diamond.rotation.z = Math.PI / 2;
     diamond.position.set(
@@ -262,7 +409,7 @@ const createText = (textData, heightOffset) => {
             size: 20,
             depth: 1
         });
-        const textMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+        const textMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, clippingPlanes: clippingPlanes });
         const textMesh = new THREE.Mesh(textGeometry, textMaterial);
         textMesh.position.set(x, y, heightOffset);
         scene.add(textMesh);
